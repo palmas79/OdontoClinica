0d57df5e556c99c76db83b088f73189a3ea6bdb3
Gerardo19D80 <gdaglio@gmail.com>
2024-03-16 18:27:05 -0300

    agregando el configure Mapping a TurnoService

--- src/main/java/com/backend/clinicaDental/service/ITurnoService.java
+++ src/main/java/com/backend/clinicaDental/service/ITurnoService.java
@@ -1,7 +1,17 @@
 package com.backend.clinicaDental.service;
 
+import com.backend.clinicaDental.dto.entrada.TurnoEntradaDto;
+import com.backend.clinicaDental.dto.salida.TurnoSalidaDto;
+import com.backend.clinicaDental.service.impl.TurnoService;
 
-//PARA HACER Y REVISAR CON FER!
+import java.util.List;
+
 public interface ITurnoService {
+
+    TurnoSalidaDto registrarTurno(TurnoEntradaDto turno);
+
+    List<TurnoSalidaDto> listarTurnos();
+
+    TurnoSalidaDto buscarTurnoPorId(Long id);
 
 }
--- src/main/java/com/backend/clinicaDental/service/impl/TurnoService.java
+++ src/main/java/com/backend/clinicaDental/service/impl/TurnoService.java
@@ -2,5 +2,57 @@ package com.backend.clinicaDental.service.impl;
 
 
 //PARA HACER Y REVISAR CON FER!
-public class TurnoService {
+
+import com.backend.clinicaDental.dto.entrada.TurnoEntradaDto;
+import com.backend.clinicaDental.dto.salida.TurnoSalidaDto;
+import com.backend.clinicaDental.entity.Turno;
+import com.backend.clinicaDental.repository.TurnoRepository;
+import com.backend.clinicaDental.service.ITurnoService;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class TurnoService implements ITurnoService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(TurnoService.class);
+    private TurnoRepository turnoRepository;
+    private ModelMapper modelMapper;
+
+    public TurnoService(TurnoRepository turnoRepository, ModelMapper modelMapper) {
+        this.turnoRepository = turnoRepository;
+        this.modelMapper = modelMapper;
+        configureMapping();
+    }
+
+    @Override
+    public TurnoSalidaDto registrarTurno(TurnoEntradaDto turno) {
+        return null;
+    }
+
+    @Override
+    public List<TurnoSalidaDto> listarTurnos() {
+        return null;
+    }
+
+    @Override
+    public TurnoSalidaDto buscarTurnoPorId(Long id) {
+        return null;
+    }
+
+    // *** METODO 4 --- CONFIGURE MAPPING --- ***
+
+    private void configureMapping() {
+
+        modelMapper.typeMap(TurnoEntradaDto.class, Turno.class)
+                .addMappings(mapper -> mapper.map(TurnoEntradaDto::getPacienteId, Turno::setPaciente))
+                .addMappings(mapper -> mapper.map(TurnoEntradaDto::getOdontologoId, Turno::setOdontologo));
+
+        modelMapper.typeMap(Turno.class, TurnoSalidaDto.class)
+                .addMappings(mapper -> mapper.map(Turno::getPaciente, TurnoSalidaDto::setPacienteSalidaDto))
+                .addMappings(mapper -> mapper.map(Turno::getOdontologo, TurnoSalidaDto::setOdontologoSalidaDto));
+    }
+
+
 }
